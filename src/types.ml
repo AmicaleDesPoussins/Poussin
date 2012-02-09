b6abc9c0c19e22713fb774907f585ad59d84846f
Yannick Zakowski <11011033@e008m09.istic.univ-rennes1.fr>
2012-02-09 14:02:30 +0100

    and, ca pue moi je dis

--- src/types.ml
+++ src/types.ml
@@ -18,7 +18,23 @@ type formula =
 module ordered_formula =
 struct 
   type t = formula
-  let compare f1 f2 = ...(*TODO*)
+  let rec compare f1 f2 = 
+    let compare_pred (s1,n1) (s2,n2) = match (n1-n2) with
+      | 0 -> String.compare(s1, s2)
+      | n where n>0 -> 1
+      | _ -> -1
+    and compare_var (x:var) (y:var) = String.compare x y
+    in
+    match (f1, f2) with
+      | (Pred p1, Pred p2) -> compare_pred p1 p2
+      | (Not f3, Not f4) -> compare f3 f4
+      | (And(f3,f4), And(f5, f6)) -> let r = compare f3 f5 in if r=0 then compare f4 f6 else r
+      | (Or(f3,f4), Or(f5, f6)) -> let r = compare f3 f5 in if r=0 then compare f4 f6 else r
+      | (Imp(f3,f4), Imp(f5, f6)) -> let r = compare f3 f5 in if r=0 then compare f4 f6 else r
+      | (Forall(x, f3), Forall(y, f4)) when x=y -> compare f3 f4
+      | (Forall(Var x, f3), Forall(Var y, f4)) -> String.compare x y
+      | (Exists(x, f3), Forall(y, f4)) when x=y -> compare f3 f4
+      | (Forall(Var x, f3), Forall(Var y, f4)) -> String.compare x y
 end
 
 module Formula_set = Set.Make ordered_formula
