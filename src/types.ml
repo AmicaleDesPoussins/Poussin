04da3b6b2a48a7991005fb610b77d7a7b15214b3
Christophe Vuillot <christophe.vuillot@eleves.bretagne.ens-cachan.fr>
2012-02-11 02:27:10 +0100

    je maintiens le lol : compare existe déjà

--- src/types.ml
+++ src/types.ml
@@ -21,34 +21,9 @@ module Ordered_formula =
 struct 
 
   type t = formula
+      
+  let compare (f1:formula) (f2:formula) = compare f1 f2
 
-  let rec compare f1 f2 =
-    let compare_pred ((s1,n1):pred) ((s2,n2):pred) = match (n1-n2) with
-      | 0 -> String.compare s1 s2
-      | n when (n>0) -> 1
-      | _ -> -1
-    and trad (f:formula) = match f with
-      | And _ -> 1
-      | Or _ -> 2
-      | Imp _ -> 3
-      | Not _ -> 4
-      | Forall _ -> 5
-      | Exists _ -> 6
-      | Pred _ -> 7
-    in
-    match (f1, f2) with
-      | (f1, f2) when (trad f1)>(trad f2) -> 1
-      | (f1, f2) when (trad f1)<(trad f2) -> -1
-      | (Pred (p1, _), Pred (p2, _)) -> compare_pred p1 p2
-      | (Not f3, Not f4) -> compare f3 f4
-      | (And(f3,f4), And(f5, f6)) -> let r = compare f3 f5 in if r=0 then compare f4 f6 else r
-      | (Or(f3,f4), Or(f5, f6)) -> let r = compare f3 f5 in if r=0 then compare f4 f6 else r
-      | (Imp(f3,f4), Imp(f5, f6)) -> let r = compare f3 f5 in if r=0 then compare f4 f6 else r
-      | (Forall(x, f3), Forall(y, f4)) when x=y -> compare f3 f4
-      | (Forall(Var x, _), Forall(Var y, _)) -> String.compare x y
-      | (Exists(x, f3), Forall(y, f4)) when x=y -> compare f3 f4
-      | (Forall(Var x, f3), Forall(Var y, f4)) -> String.compare x y
-      | _ -> failwith "compare"
 end
 
 
