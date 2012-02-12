1c2c6f9cbbcbd614d3a57d91718d479a711ece75
Simon Boulier <simon.boulier@gmail.com>
2012-02-12 01:17:17 +0100

    bien joué le compare, proposition de proof_tree... qui marche pas encore :-)

--- src/types.ml
+++ src/types.ml
@@ -15,23 +15,39 @@ type formula =
   | Forall of var * formula
   | Exists of var * formula
 
-
-
 module Ordered_formula =
 struct 
-
   type t = formula
-      
-  let compare (f1:formula) (f2:formula) = compare f1 f2
-
+  let compare = compare
 end
-
-
 module Formula_set = Set.Make(Ordered_formula)
 
-type sequent = {context : Formula_set.t ; goal : formula}
 
-type rules = sequent list -> sequent (*A modifier, arité incluse ? Toujours le même type quelque soit la théorie ?*)
+type sequent = {context : Formula_set.t ; goal : formula}
+module Ordered_sequent =
+struct
+  type t = sequent
+  let compare = compare
+end
+module Sequent_set = Set.Make(Ordered_sequent)
+
+module Rules =
+struct
+  type rules = e_and | i_and | e_or | i_or
+  let check r p c = true
+end
+
+(*module proof_tree = functor (R : Rules)
+struct
+  type t =
+    | Nil
+    | Leaf of Rules.rules * Sequent_set.t * sequent
+  val is_valid : t -> bool
+end
+*)
+
+
+(*type rules = Sequent_set.t -> sequent  A modifier, arité incluse ? Toujours le même type quelque soit la théorie ?*)
 
 (*
 struct set_of_rules
--- src/types.mli
+++ src/types.mli
@@ -2,6 +2,7 @@ type var
 type func
 type pred
 type term = Var of var | Func of func * term list
+
 type formula =
     Pred of pred * term list
   | Not of formula
@@ -11,7 +12,7 @@ type formula =
   | Forall of var * formula
   | Exists of var * formula
 module Ordered_formula :
-  sig type t = formula val compare : formula -> formula -> int end
+  sig type t = formula val compare : 'a -> 'a -> int end
 module Formula_set :
   sig
     type elt = Ordered_formula.t
@@ -41,5 +42,42 @@ module Formula_set :
     val choose : t -> elt
     val split : elt -> t -> t * bool * t
   end
+
 type sequent = { context : Formula_set.t; goal : formula; }
-type rules = sequent list -> sequent
+module Ordered_sequent :
+  sig type t = sequent val compare : 'a -> 'a -> int end
+module Sequent_set :
+  sig
+    type elt = Ordered_sequent.t
+    type t = Set.Make(Ordered_sequent).t
+    val empty : t
+    val is_empty : t -> bool
+    val mem : elt -> t -> bool
+    val add : elt -> t -> t
+    val singleton : elt -> t
+    val remove : elt -> t -> t
+    val union : t -> t -> t
+    val inter : t -> t -> t
+    val diff : t -> t -> t
+    val compare : t -> t -> int
+    val equal : t -> t -> bool
+    val subset : t -> t -> bool
+    val iter : (elt -> unit) -> t -> unit
+    val fold : (elt -> 'a -> 'a) -> t -> 'a -> 'a
+    val for_all : (elt -> bool) -> t -> bool
+    val exists : (elt -> bool) -> t -> bool
+    val filter : (elt -> bool) -> t -> t
+    val partition : (elt -> bool) -> t -> t * t
+    val cardinal : t -> int
+    val elements : t -> elt list
+    val min_elt : t -> elt
+    val max_elt : t -> elt
+    val choose : t -> elt
+    val split : elt -> t -> t * bool * t
+  end
+
+module Rules :
+sig
+  type rules
+  val check : rules -> Sequent_set.t -> sequent -> bool
+end
