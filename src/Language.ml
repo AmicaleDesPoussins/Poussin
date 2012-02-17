5bf9ae8164e7e8270ed2eb33cb305f93c0f973f3
Christophe Vuillot <11011025@e008m10.istic.univ-rennes1.fr>
2012-02-17 13:06:58 +0100

    module language, humhum

---
+++ src/Language.ml
@@ -0,0 +1,9 @@
+module type Language = 
+sig
+  type connector
+  type var
+  type quant
+  type func
+  type pred
+  type formula
+end
---
+++ src/Language.mli
@@ -0,0 +1,2 @@
+module type Language =
+  sig type connector type var type quant type func type pred type formula end
